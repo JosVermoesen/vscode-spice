383717a69d6214be883076415a9dc5eff03f539c
Jos Vermoesen <info@rv.be>
2019-02-01 17:26:23 +0100

    Coupon functionality

---
+++ Areas/Admin/Controllers/CouponController.cs
@@ -0,0 +1,153 @@
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using vscode_spice.Data;
+using vscode_spice.Models;
+
+namespace vscode_spice.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CouponController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CouponController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _db.Coupon.ToListAsync());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Coupon couponNew)
+        {
+            if (ModelState.IsValid)
+            {
+                var files = HttpContext.Request.Form.Files;
+                if (files.Count > 0)
+                {
+                    byte[] p1 = null;
+                    using (var fs1 = files[0].OpenReadStream())
+                    {
+                        using (var ms1 = new MemoryStream())
+                        {
+                            fs1.CopyTo(ms1);
+                            p1 = ms1.ToArray();
+                        }
+                    }
+                    couponNew.Picture = p1;
+                }
+                _db.Coupon.Add(couponNew);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(couponNew);
+        }
+
+        //GET Edit Coupon
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var coupon = await _db.Coupon.SingleOrDefaultAsync(m => m.Id == id);
+            if (coupon == null)
+            {
+                return NotFound();
+            }
+            return View(coupon);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Coupon couponEdit)
+        {
+            if (couponEdit.Id == 0)
+            {
+                return NotFound();
+            }
+
+            var couponFromDb = await _db.Coupon.Where(c => c.Id == couponEdit.Id).FirstOrDefaultAsync();
+
+            if (ModelState.IsValid)
+            {
+                var files = HttpContext.Request.Form.Files;
+                if (files.Count > 0)
+                {
+                    byte[] p1 = null;
+                    using (var fs1 = files[0].OpenReadStream())
+                    {
+                        using (var ms1 = new MemoryStream())
+                        {
+                            fs1.CopyTo(ms1);
+                            p1 = ms1.ToArray();
+                        }
+                    }
+                    couponFromDb.Picture = p1;
+                }
+                couponFromDb.MinimumAmount = couponEdit.MinimumAmount;
+                couponFromDb.Name = couponEdit.Name;
+                couponFromDb.Discount = couponEdit.Discount;
+                couponFromDb.CouponType = couponEdit.CouponType;
+                couponFromDb.IsActive = couponEdit.IsActive;
+
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(couponEdit);
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var coupon = await _db.Coupon
+               .FirstOrDefaultAsync(m => m.Id == id);
+            if (coupon == null)
+            {
+                return NotFound();
+            }
+
+            return View(coupon);
+        }
+
+        //GET Delete Coupon
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var coupon = await _db.Coupon.SingleOrDefaultAsync(m => m.Id == id);
+            if (coupon == null)
+            {
+                return NotFound();
+            }
+            return View(coupon);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var coupons = await _db.Coupon.SingleOrDefaultAsync(m => m.Id == id);
+            _db.Coupon.Remove(coupons);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Models/Coupon.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace vscode_spice.Models
+{
+    public class Coupon
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string CouponType { get; set; }
+        public enum ECouponType { Percent = 0, Dollar = 1 }
+        [Required]
+        public double Discount { get; set; }
+        [Required]
+        public double MinimumAmount { get; set; }
+        public byte[] Picture { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
