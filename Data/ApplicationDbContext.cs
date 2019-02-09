e2aefc3da22e3181cb8d0aa2fe90c829966d4370
Jos Vermoesen <info@rv.be>
2019-02-09 13:29:30 +0100

    order view start

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -16,25 +16,23 @@ namespace vscode_spice.Data
 
         public DbSet<Category> Category { get; set; }
         // dotnet ef migrations add addCategoryToDatabase
-
         public DbSet<SubCategory> SubCategory { get; set; }
         // dotnet ef migrations add addSubCategoryToDatabase
-
         public DbSet<MenuItem> MenuItem { get; set; }
         // dotnet ef migrations add addMenuItemToDatabase
         // WARNING
         // Gives error when using SQL Server but not on SLQ Lite
         // Change onDelete: ReferentialAction.NoAction);
         // before database update
-
         public DbSet<Coupon> Coupon { get; set; }
         // dotnet ef migrations add addCouponToDatabase
-
         public DbSet<ApplicationUser> ApplicationUser { get; set; }
         // dotnet ef migrations add addMoreFieldsToIdentityUser
-
         public DbSet<ShoppingCart> ShoppingCart { get; set; }
         // dotnet ef migrations add addShoppingCartToDatabase
+        public DbSet<OrderHeader> OrderHeader { get; set; }
+        public DbSet<OrderDetails> OrderDetails { get; set; }
+        // dotnet ef migrations add addOrderHeaderAndDetailsToDB
         // dotnet ef database update
     }
 }
--- Migrations/20190209082943_addOrderHeaderAndDetailsToDB.Designer.cs
+++ Migrations/20190209082943_addOrderHeaderAndDetailsToDB.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using vscode_spice.Data;
 
 namespace vscode_spice.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190209082943_addOrderHeaderAndDetailsToDB")]
+    partial class addOrderHeaderAndDetailsToDB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
