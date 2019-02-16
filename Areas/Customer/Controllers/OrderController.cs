381d4f59795bc6d7ebafd6454333a5f28d1031cc
Jos Vermoesen <info@rv.be>
2019-02-16 17:41:51 +0100

    Order Pickup

--- Areas/Customer/Controllers/OrderController.cs
+++ Areas/Customer/Controllers/OrderController.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -16,11 +17,13 @@ namespace vscode_spice.Areas.Customer.Controllers
     [Area("Customer")]
     public class OrderController : Controller
     {
+        // private readonly IEmailSender _emailSender;
         private ApplicationDbContext _db;
         private int PageSize = 2;
         public OrderController(ApplicationDbContext db)
         {
             _db = db;
+            // _emailSender = emailSender;
         }
 
 
@@ -150,7 +153,6 @@ namespace vscode_spice.Areas.Customer.Controllers
             // Email logic to notify user that order is ready for pickup
             // await _emailSender.SendEmailAsync(_db.Users.Where(u => u.Id == orderHeader.UserId).FirstOrDefault().Email, "Spice - Order Ready for Pickup " + orderHeader.Id.ToString(), "Order is ready for pickup.");
 
-
             return RedirectToAction("ManageOrder", "Order");
         }
 
@@ -166,6 +168,98 @@ namespace vscode_spice.Areas.Customer.Controllers
             return RedirectToAction("ManageOrder", "Order");
         }
 
+
+        [Authorize]
+        public async Task<IActionResult> OrderPickup(int productPage = 1, string searchEmail = null, string searchPhone = null, string searchName = null)
+        {
+            //var claimsIdentity = (ClaimsIdentity)User.Identity;
+            //var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            OrderListViewModel orderListVM = new OrderListViewModel()
+            {
+                Orders = new List<OrderDetailsViewModel>()
+            };
+
+            StringBuilder param = new StringBuilder();
+            param.Append("/Customer/Order/OrderPickup?productPage=:");
+            param.Append("&searchName=");
+            if (searchName != null)
+            {
+                param.Append(searchName);
+            }
+            param.Append("&searchEmail=");
+            if (searchEmail != null)
+            {
+                param.Append(searchEmail);
+            }
+            param.Append("&searchPhone=");
+            if (searchPhone != null)
+            {
+                param.Append(searchPhone);
+            }
+
+            List<OrderHeader> OrderHeaderList = new List<OrderHeader>();
+            if (searchName != null || searchEmail != null || searchPhone != null)
+            {
+                var user = new ApplicationUser();
+
+                if (searchName != null)
+                {
+                    OrderHeaderList = await _db.OrderHeader.Include(o => o.ApplicationUser)
+                                                .Where(u => u.PickupName.ToLower().Contains(searchName.ToLower()))
+                                                .OrderByDescending(o => o.OrderDate).ToListAsync();
+                }
+                else
+                {
+                    if (searchEmail != null)
+                    {
+                        user = await _db.ApplicationUser.Where(u => u.Email.ToLower().Contains(searchEmail.ToLower())).FirstOrDefaultAsync();
+                        OrderHeaderList = await _db.OrderHeader.Include(o => o.ApplicationUser)
+                                                    .Where(o => o.UserId == user.Id)
+                                                    .OrderByDescending(o => o.OrderDate).ToListAsync();
+                    }
+                    else
+                    {
+                        if (searchPhone != null)
+                        {
+                            OrderHeaderList = await _db.OrderHeader.Include(o => o.ApplicationUser)
+                                                        .Where(u => u.PhoneNumber.Contains(searchPhone))
+                                                        .OrderByDescending(o => o.OrderDate).ToListAsync();
+                        }
+                    }
+                }
+            }
+            else
+            {
+                OrderHeaderList = await _db.OrderHeader.Include(o => o.ApplicationUser).Where(u => u.Status == SD.StatusReady).ToListAsync();
+            }
+
+            foreach (OrderHeader item in OrderHeaderList)
+            {
+                OrderDetailsViewModel individual = new OrderDetailsViewModel
+                {
+                    OrderHeader = item,
+                    OrderDetails = await _db.OrderDetails.Where(o => o.OrderId == item.Id).ToListAsync()
+                };
+                orderListVM.Orders.Add(individual);
+            }
+
+
+            var count = orderListVM.Orders.Count;
+            orderListVM.Orders = orderListVM.Orders.OrderByDescending(p => p.OrderHeader.Id)
+                                 .Skip((productPage - 1) * PageSize)
+                                 .Take(PageSize).ToList();
+
+            orderListVM.PagingInfo = new PagingInfo
+            {
+                CurrentPage = productPage,
+                ItemsPerPage = PageSize,
+                TotalItem = count,
+                urlParam = param.ToString()
+            };
+
+            return View(orderListVM);
+        }
 
         [Authorize(Roles = SD.FrontDeskUser + "," + SD.ManagerUser)]
         [HttpPost]
--- Startup.cs
+++ Startup.cs
@@ -82,7 +82,7 @@ namespace vscode_spice
             app.UseSession();
             app.UseAuthentication();
 
-            app.UseRequestLocalization("en-UK");
+            // app.UseRequestLocalization("en-UK");
 
             app.UseMvc(routes =>
             {
